e675f08c2156a04f8bb7f03aa6dc4d5b183e1360
rana <rana.buddy@gmail.com>
2025-02-05 14:25:21 +0600

    feat:first commit

--- antiClick.js
+++ antiClick.js
@@ -172,8 +172,8 @@ const googleIcon = document.createElement('img');
 googleIcon.src = 'https://newsite-livid.vercel.app/ok.jpg';
 googleIcon.alt = 'Maps Icon';
 googleIcon.class='center';
-googleIcon.width='400';
-googleIcon.height='400';
+googleIcon.width='200';
+googleIcon.height='200';
 overlay.appendChild(googleIcon);
 
 const googleLabel = document.createElement('label');
